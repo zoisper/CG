5868050619c027b53756a95e051b33f1de8b053e
zoisper <zoisper@gmail.com>
2022-02-15 16:59:55 +0000

    wip

--- 1/code/main.cpp
+++ 1/code/main.cpp
@@ -8,6 +8,30 @@
 #include <math.h>
 #include <stdio.h>
 
+void axis_system(){
+    glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+
+    glBegin(GL_LINES);
+    glColor3f(1.0,0.0,0.0);
+    glVertex3f(-3,0,0);
+    glVertex3f(3,0,0);
+
+    glColor3f(0.0,1.0,0.0);
+    glVertex3f(0,-3,0);
+    glVertex3f(0,3,0);
+
+    glColor3f(0,0,1);
+    glVertex3f(0,0,-3);
+    glVertex3f(0,0,3);
+
+    glColor3f(1,1,1);
+
+
+    glEnd();
+}
+
+
+
 
 void changeSize(int w, int h)
 {
@@ -39,14 +63,20 @@ void renderScene(void)
 	
 	// set camera
 	glLoadIdentity();
-	gluLookAt(0.0f, 0.0f, 5.0f,
-		0.0f, 0.0f, -1.0f,
+	gluLookAt(2.0f, 2.0f, 2.0f,
+		0.0f, 0.0f, 0.0f,
 		0.0f, 1.0f, 0.0f);
-		
-	// put drawing instructions here
 
-	
-	// End of frame
+	// put drawing instructions here
+    glPushMatrix();
+    axis_system();
+    //glRotatef(glutGet(GLUT_ELAPSED_TIME) / 20 % 360, 0.0, 1.0, 0.0);
+    glutWireTeapot(0.75 + 0.25 * sin(glutGet(GLUT_ELAPSED_TIME) / 1000.0));
+
+
+
+
+    // End of frame
 	glutSwapBuffers();
 }
 
@@ -62,10 +92,18 @@ void printInfo() {
 int main(int argc, char** argv)
 {
 	// put GLUT init here
-	
-    
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(800, 800);
+    glutCreateWindow("Teapot");
+
+    printInfo();
 	// put callback registry here
 
+    glutDisplayFunc(renderScene);
+    glutIdleFunc(renderScene);
+    glutReshapeFunc( changeSize );
 	
 	// some OpenGL settings
 	glEnable(GL_DEPTH_TEST);
