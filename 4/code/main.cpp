68251297c2876f47e3534e90898def5ad6d788ff
zoisper <zoisper@gmail.com>
2022-02-25 00:03:12 +0000

    wip

--- 4/code/main.cpp
+++ 4/code/main.cpp
@@ -9,46 +9,51 @@
 
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <vector>
+
+GLuint vertices, verticeCount;
+std::vector<double> vertexB;
 
 float alfa = 0.0f, beta = 0.0f, radius = 5.0f;
 float camX, camY, camZ;
+int mode = GL_LINE;
 
 
 void spherical2Cartesian() {
 
-	camX = radius * cos(beta) * sin(alfa);
-	camY = radius * sin(beta);
-	camZ = radius * cos(beta) * cos(alfa);
+    camX = radius * cos(beta) * sin(alfa);
+    camY = radius * sin(beta);
+    camZ = radius * cos(beta) * cos(alfa);
 }
 
 
 void changeSize(int w, int h) {
 
-	// Prevent a divide by zero, when window is too short
-	// (you cant make a window with zero width).
-	if(h == 0)
-		h = 1;
+    // Prevent a divide by zero, when window is too short
+    // (you cant make a window with zero width).
+    if(h == 0)
+        h = 1;
 
-	// compute window's aspect ratio 
-	float ratio = w * 1.0 / h;
+    // compute window's aspect ratio
+    float ratio = w * 1.0 / h;
 
-	// Set the projection matrix as current
-	glMatrixMode(GL_PROJECTION);
-	// Load Identity Matrix
-	glLoadIdentity();
-	
-	// Set the viewport to be the entire window
+    // Set the projection matrix as current
+    glMatrixMode(GL_PROJECTION);
+    // Load Identity Matrix
+    glLoadIdentity();
+
+    // Set the viewport to be the entire window
     glViewport(0, 0, w, h);
 
-	// Set perspective
-	gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
+    // Set perspective
+    gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
 
-	// return to the model view matrix mode
-	glMatrixMode(GL_MODELVIEW);
+    // return to the model view matrix mode
+    glMatrixMode(GL_MODELVIEW);
 }
 
 
-/*----------------------------------------------------------------------------------- 
+/*-----------------------------------------------------------------------------------
 	Draw Cylinder with strips and fans
 
 	  parameters: radius, height, sides
@@ -58,37 +63,37 @@ void changeSize(int w, int h) {
 
 void cylinder0(float radius, float height, int sides) {
 
-	int i;
-	float step;
+    int i;
+    float step;
 
-	step = 360.0/sides;
+    step = 360.0/sides;
 
-	glColor3f(1,0,0);
-	glBegin(GL_TRIANGLE_FAN);
+    glColor3f(1,0,0);
+    glBegin(GL_TRIANGLE_FAN);
 
-		glVertex3f(0,height*0.5,0);
-		for (i=0; i <= sides; i++) {
-			glVertex3f(cos(i * step * M_PI/180.0)*radius,height*0.5,-sin(i * step *M_PI/180.0)*radius);
-		}
-	glEnd();
+    glVertex3f(0,height*0.5,0);
+    for (i=0; i <= sides; i++) {
+        glVertex3f(cos(i * step * M_PI/180.0)*radius,height*0.5,-sin(i * step *M_PI/180.0)*radius);
+    }
+    glEnd();
 
-	glColor3f(0,1,0);
-	glBegin(GL_TRIANGLE_FAN);
+    glColor3f(0,1,0);
+    glBegin(GL_TRIANGLE_FAN);
 
-		glVertex3f(0,-height*0.5,0);
-		for (i=0; i <= sides; i++) {
-			glVertex3f(cos(i * step * M_PI/180.0)*radius,-height*0.5,sin(i * step *M_PI/180.0)*radius);
-		}
-	glEnd();
+    glVertex3f(0,-height*0.5,0);
+    for (i=0; i <= sides; i++) {
+        glVertex3f(cos(i * step * M_PI/180.0)*radius,-height*0.5,sin(i * step *M_PI/180.0)*radius);
+    }
+    glEnd();
 
-	glColor3f(0,0,1);
-	glBegin(GL_TRIANGLE_STRIP);
+    glColor3f(0,0,1);
+    glBegin(GL_TRIANGLE_STRIP);
 
-		for (i=0; i <= sides; i++) {
-			glVertex3f(cos(i * step * M_PI/180.0)*radius, height*0.5,-sin(i * step *M_PI/180.0)*radius);
-			glVertex3f(cos(i * step * M_PI/180.0)*radius,-height*0.5,-sin(i * step *M_PI/180.0)*radius);
-		}
-	glEnd();
+    for (i=0; i <= sides; i++) {
+        glVertex3f(cos(i * step * M_PI/180.0)*radius, height*0.5,-sin(i * step *M_PI/180.0)*radius);
+        glVertex3f(cos(i * step * M_PI/180.0)*radius,-height*0.5,-sin(i * step *M_PI/180.0)*radius);
+    }
+    glEnd();
 }
 
 
@@ -102,145 +107,249 @@ void cylinder0(float radius, float height, int sides) {
 
 void cylinder(float radius, float height, int sides) {
 
-	int i;
-	float step;
+    int i;
+    float step;
 
-	step = 360.0 / sides;
+    step = 360.0 / sides;
 
-	glBegin(GL_TRIANGLES);
+    glBegin(GL_TRIANGLES);
 
-	// top
-	for (i = 0; i < sides; i++) {
-		glVertex3f(0, height*0.5, 0);
-		glVertex3f(cos(i * step * M_PI / 180.0)*radius, height*0.5, -sin(i * step *M_PI / 180.0)*radius);
-		glVertex3f(cos((i+1) * step * M_PI / 180.0)*radius, height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
-	}
+    // top
+    for (i = 0; i < sides; i++) {
+        glVertex3f(0, height*0.5, 0);
+        glVertex3f(cos(i * step * M_PI / 180.0)*radius, height*0.5, -sin(i * step *M_PI / 180.0)*radius);
+        glVertex3f(cos((i+1) * step * M_PI / 180.0)*radius, height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
+    }
 
-	// bottom
-	for (i = 0; i < sides; i++) {
-		glVertex3f(0, -height*0.5, 0);
-		glVertex3f(cos((i + 1) * step * M_PI / 180.0)*radius, -height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
-		glVertex3f(cos(i * step * M_PI / 180.0)*radius, -height*0.5, -sin(i * step *M_PI / 180.0)*radius);
-	}
+    // bottom
+    for (i = 0; i < sides; i++) {
+        glVertex3f(0, -height*0.5, 0);
+        glVertex3f(cos((i + 1) * step * M_PI / 180.0)*radius, -height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
+        glVertex3f(cos(i * step * M_PI / 180.0)*radius, -height*0.5, -sin(i * step *M_PI / 180.0)*radius);
+    }
 
-	// body
-	for (i = 0; i <= sides; i++) {
+    // body
+    for (i = 0; i <= sides; i++) {
 
-		glVertex3f(cos(i * step * M_PI / 180.0)*radius, height*0.5, -sin(i * step *M_PI / 180.0)*radius);
-		glVertex3f(cos(i * step * M_PI / 180.0)*radius, -height*0.5, -sin(i * step *M_PI / 180.0)*radius);
-		glVertex3f(cos((i + 1) * step * M_PI / 180.0)*radius, height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
+        glVertex3f(cos(i * step * M_PI / 180.0)*radius, height*0.5, -sin(i * step *M_PI / 180.0)*radius);
+        glVertex3f(cos(i * step * M_PI / 180.0)*radius, -height*0.5, -sin(i * step *M_PI / 180.0)*radius);
+        glVertex3f(cos((i + 1) * step * M_PI / 180.0)*radius, height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
 
-		glVertex3f(cos(i * step * M_PI / 180.0)*radius, -height*0.5, -sin(i * step *M_PI / 180.0)*radius);
-		glVertex3f(cos((i + 1) * step * M_PI / 180.0)*radius, -height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
-		glVertex3f(cos((i + 1) * step * M_PI / 180.0)*radius, height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
-	}
-	glEnd();
+        glVertex3f(cos(i * step * M_PI / 180.0)*radius, -height*0.5, -sin(i * step *M_PI / 180.0)*radius);
+        glVertex3f(cos((i + 1) * step * M_PI / 180.0)*radius, -height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
+        glVertex3f(cos((i + 1) * step * M_PI / 180.0)*radius, height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
+    }
+    glEnd();
 }
+
+void vboCylinder(float radius, float height, int sides) {
+    vertexB.clear();
+    int i;
+    float step;
+
+    step = 360.0 / sides;
+
+
+
+    // top
+    for (i = 0; i < sides; i++) {
+        vertexB.push_back(0);
+        vertexB.push_back(height*0.5);
+        vertexB.push_back(0);
+
+        vertexB.push_back(cos(i * step * M_PI / 180.0)*radius);
+        vertexB.push_back(height*0.5);
+        vertexB.push_back(-sin(i * step *M_PI / 180.0)*radius);
+
+        vertexB.push_back(cos((i+1) * step * M_PI / 180.0)*radius);
+        vertexB.push_back(height*0.5);
+        vertexB.push_back(-sin((i + 1) * step *M_PI / 180.0)*radius);
+
+
+    }
+
+    // bottom
+    for (i = 0; i < sides; i++) {
+        vertexB.push_back(0);
+        vertexB.push_back(-height*0.5);
+        vertexB.push_back(0);
+
+        vertexB.push_back(cos((i + 1) * step * M_PI / 180.0)*radius);
+        vertexB.push_back(-height*0.5);
+        vertexB.push_back(-sin((i + 1) * step *M_PI / 180.0)*radius);
+
+        vertexB.push_back(cos(i * step * M_PI / 180.0)*radius);
+        vertexB.push_back(-height*0.5);
+        vertexB.push_back(-sin(i * step *M_PI / 180.0)*radius);
+
+    }
+
+    // body
+    for (i = 0; i <= sides; i++) {
+
+        vertexB.push_back(cos(i * step * M_PI / 180.0)*radius);
+        vertexB.push_back(height*0.5);
+        vertexB.push_back(-sin(i * step *M_PI / 180.0)*radius);
+
+        vertexB.push_back(cos(i * step * M_PI / 180.0)*radius);
+        vertexB.push_back(-height*0.5);
+        vertexB.push_back(-sin(i * step *M_PI / 180.0)*radius);
+
+        vertexB.push_back(cos((i + 1) * step * M_PI / 180.0)*radius);
+        vertexB.push_back(height*0.5);
+        vertexB.push_back(-sin((i + 1) * step *M_PI / 180.0)*radius);
+
+        vertexB.push_back(cos(i * step * M_PI / 180.0)*radius);
+        vertexB.push_back(-height*0.5);
+        vertexB.push_back(-sin(i * step *M_PI / 180.0)*radius);
+
+        vertexB.push_back(cos((i + 1) * step * M_PI / 180.0)*radius);
+        vertexB.push_back(-height*0.5);
+        vertexB.push_back(-sin((i + 1) * step *M_PI / 180.0)*radius);
+
+        vertexB.push_back(cos((i + 1) * step * M_PI / 180.0)*radius);
+        vertexB.push_back(height*0.5);
+        vertexB.push_back(-sin((i + 1) * step *M_PI / 180.0)*radius);
+
+        verticeCount = vertexB.size()/3;
+        glBindBuffer(GL_ARRAY_BUFFER, vertices);
+        glBufferData(GL_ARRAY_BUFFER, vertexB.size() * sizeof(double), vertexB.data(), GL_STATIC_DRAW);
+    }
+}
+
 
 
 void renderScene(void) {
 
-	// clear buffers
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // clear buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// set the camera
-	glLoadIdentity();
-	gluLookAt(camX, camY, camZ,
-		0.0, 0.0, 0.0,
-		0.0f, 1.0f, 0.0f);
+    // set the camera
+    glLoadIdentity();
+    gluLookAt(camX, camY, camZ,
+              0.0, 0.0, 0.0,
+              0.0f, 1.0f, 0.0f);
 
-	cylinder(1,2,10);
+    //cylinder(1,2,10);
+    glPolygonMode(GL_FRONT, mode);
+    glColor3f(0.0,1.0,1.0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertices);
+    glVertexPointer(3, GL_DOUBLE, 0, 0);
+    glDrawArrays(GL_TRIANGLES, 0, verticeCount);
 
-	// End of frame
-	glutSwapBuffers();
+
+    // End of frame
+    glutSwapBuffers();
 }
 
 
 void processKeys(unsigned char c, int xx, int yy) {
 
 // put code to process regular keys in here
+    switch(c){
+        case 'l':
+            mode = GL_LINE;
+            break;
+        case 'p':
+            mode = GL_POINT;
+            break;
+        case 'f':
+            mode = GL_FILL;
+            break;
+        default:
+            break;
+    }
+    glutPostRedisplay();
 
 }
 
 
 void processSpecialKeys(int key, int xx, int yy) {
 
-	switch (key) {
+    switch (key) {
 
-	case GLUT_KEY_RIGHT:
-		alfa -= 0.1; break;
+        case GLUT_KEY_RIGHT:
+            alfa -= 0.1; break;
 
-	case GLUT_KEY_LEFT:
-		alfa += 0.1; break;
+        case GLUT_KEY_LEFT:
+            alfa += 0.1; break;
 
-	case GLUT_KEY_UP:
-		beta += 0.1f;
-		if (beta > 1.5f)
-			beta = 1.5f;
-		break;
+        case GLUT_KEY_UP:
+            beta += 0.1f;
+            if (beta > 1.5f)
+                beta = 1.5f;
+            break;
 
-	case GLUT_KEY_DOWN:
-		beta -= 0.1f;
-		if (beta < -1.5f)
-			beta = -1.5f;
-		break;
+        case GLUT_KEY_DOWN:
+            beta -= 0.1f;
+            if (beta < -1.5f)
+                beta = -1.5f;
+            break;
 
-	case GLUT_KEY_PAGE_DOWN: radius -= 0.1f;
-		if (radius < 0.1f)
-			radius = 0.1f;
-		break;
+        case GLUT_KEY_PAGE_DOWN: radius -= 0.1f;
+            if (radius < 0.1f)
+                radius = 0.1f;
+            break;
 
-	case GLUT_KEY_PAGE_UP: radius += 0.1f; break;
-	}
-	spherical2Cartesian();
-	glutPostRedisplay();
+        case GLUT_KEY_PAGE_UP: radius += 0.1f; break;
+    }
+    spherical2Cartesian();
+    glutPostRedisplay();
 
 }
 
 
 void printInfo() {
 
-	printf("Vendor: %s\n", glGetString(GL_VENDOR));
-	printf("Renderer: %s\n", glGetString(GL_RENDERER));
-	printf("Version: %s\n", glGetString(GL_VERSION));
+    printf("Vendor: %s\n", glGetString(GL_VENDOR));
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+    printf("Version: %s\n", glGetString(GL_VERSION));
 
-	printf("\nUse Arrows to move the camera up/down and left/right\n");
-	printf("Home and End control the distance from the camera to the origin");
+    printf("\nUse Arrows to move the camera up/down and left/right\n");
+    printf("Home and End control the distance from the camera to the origin");
 }
 
 
 int main(int argc, char **argv) {
 
 // init GLUT and the window
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
-	glutInitWindowPosition(100,100);
-	glutInitWindowSize(800,800);
-	glutCreateWindow("CG@DI-UM");
-		
-// Required callback registry 
-	glutDisplayFunc(renderScene);
-	glutReshapeFunc(changeSize);
-	
-// Callback registration for keyboard processing
-	glutKeyboardFunc(processKeys);
-	glutSpecialFunc(processSpecialKeys);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(800,800);
+    glutCreateWindow("CG@DI-UM");
 
-#ifndef __APPLE__
-	glewInit();
-#endif
+// Required callback registry
+    glutDisplayFunc(renderScene);
+    glutReshapeFunc(changeSize);
+
+
+    glewInit();
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glGenBuffers(1, &vertices);
+    vboCylinder(1,2,10);
+
+
+// Callback registration for keyboard processing
+    glutKeyboardFunc(processKeys);
+    glutSpecialFunc(processSpecialKeys);
+
+
+
+
+
 
 //  OpenGL settings
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
-	glPolygonMode(GL_FRONT, GL_LINE);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    //glPolygonMode(GL_FRONT, mode);
 
-	spherical2Cartesian();
+    spherical2Cartesian();
 
-	printInfo();
+    printInfo();
 
 // enter GLUT's main cycle
-	glutMainLoop();
-	
-	return 1;
+    glutMainLoop();
+
+    return 1;
 }
